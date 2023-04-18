eaee81ee8eb35d27033382c6f1f464b27e59f4ed
Rahul Sohaliya <Rahulsohaliya1@gmail.com>
2023-04-19 01:33:22 +0530

    Initial Commit

---
+++ SirUserApiCallin/OrderTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  OrderTableViewCell.swift
+//  UserApiCall
+//
+//  Created by Rahul Sohaliya on 13/02/23.
+//
+
+import UIKit
+
+class OrderTableViewCell: UITableViewCell {
+
+    @IBOutlet var reOrderButton: UIButton!
+    @IBOutlet var cancelOrderButton: UIButton!
+    @IBOutlet weak var totalAmount: UILabel!
+    @IBOutlet weak var vendorName: UILabel!
+    @IBOutlet weak var vImage: UIImageView!
+    @IBOutlet weak var orderStatus: UILabel!
+    @IBOutlet weak var date_Time: UILabel!
+    @IBOutlet weak var orderId: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
